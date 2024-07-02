94a71ced421809d61db9ee9e58339e4e06118222
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-07-02 19:08:21 +0600

    newsportal theme

--- while.php
+++ while.php
@@ -1,21 +1,128 @@
 <?php
+// $name="0";
 
-// $post_num= wp_count_posts()->publish;
-// echo $post_num."<br>";
+//     while($name<1000){
+//     echo $name." Rasel is a boot <br>";
+//     $name++;
+// }
 
 
-// $postcount =$post_num;
+
+
+
+// function name(){
+//     echo "allah mohan";
+// }
+// name();
+
+
+// function greet($name) {
+//     echo "Hello, $name!";
+// }
+
+// // Calling the function with a parameter
+// greet("Alice"); // Outputs: Hello, Alice!
+
+
+// $postcount =0;
 // while($postcount<4){
 //     echo $postcount."<br>";
 //     $postcount++;
 // }
 
 
-$postcount =0;
-while($postcount<4){
-    echo $postcount."<br>";
-    $postcount++;
-}
-    
+// the loop
 
+// while(have_posts()):the_post();
+// the_title()."<br>";
+// endwhile;
+
+
+// while(have_posts()):the_post();
+// echo get_the_title()."<br>";
+// endwhile;
+
+// while(have_posts()):the_post();
+// the_author()."<br>";
+// endwhile;
+
+
+// while (have_posts()):the_post();
+// next_post_link();
+// endwhile;
+
+
+// while(have_posts()):the_post();
+// previous_post_link();
+// endwhile;
+
+
+// while(have_posts()):the_post();
+// the_category();
+// endwhile;
+
+
+
+// while(have_posts()):the_post();
+// echo get_the_author()."<br>";
+// endwhile;
+
+
+
+// while(have_posts()):the_post();
+// the_content();
+// endwhile;
+
+
+
+
+// while(have_posts()):the_post();
+// the_excerpt();
+// endwhile;
+
+
+
+// while(have_posts()):the_post();
+// echo get_the_id()."<br>";
+// endwhile;
+
+
+
+// while(have_posts()):the_post();
+// the_meta();
+// endwhile;
+
+
+
+// while(have_posts()):the_post();
+// the_shortlink();
+// endwhile;
+
+
+
+
+
+// while(have_posts()):the_post();
+// the_tags();
+// endwhile;
+
+
+
+
+// while(have_posts()):the_post();
+// the_title();
+// endwhile;
+
+
+
+
+while(have_posts()):the_post();
+the_author();
+endwhile;
+
+
+
+while(have_posts()):the_post();
+the_content();
+endwhile;
 ?>
